01de9b3896dbe1bae60d3555dc67c2ccb667b2b2
Gaurav Shokhanda <gauravshokhanda2@gmail.com>
2025-12-02 16:45:58 +0530

    new

--- app/dashboard/student/sessions/page.tsx
+++ app/dashboard/student/sessions/page.tsx
@@ -454,7 +454,7 @@ export default function StudentSessions() {
                       variant="outline"
                       onClick={() => openSessionsModal(s.regularClassId)}
                     >
-                      View Sessionsss
+                      View Sessions
                     </Button>
                   )}
                 </div>
